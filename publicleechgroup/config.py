45641515b4dad949d31314a128f718b65f034495
shamimalshagunasekara <96081451+shamimalshagunasekara@users.noreply.github.com>
2021-12-30 16:33:13 +0530

    Update config.py

--- publicleechgroup/config.py
+++ publicleechgroup/config.py
@@ -18,3 +18,19 @@ load_dotenv("config.env")
 
 
 class Config:
+    # get a token from @BotFather
+    TG_BOT_TOKEN = 5005267086:AAEH1StoLvb4CiY7-uV_LV4RXX_QVow6MzI
+    # The Telegram API things
+    APP_ID = 1424314
+    API_HASH = d1c1c9262bbae8f5eeb80ba47c9f3dff
+    # Get these values from my.telegram.org
+    # array to store the channel ID who are authorized to use the bot
+    AUTH_CHANNEL = -1001410465876 
+    # the download location, where the HTTP Server runs
+    # Telegram maximum file upload siz
+    # chunk size that should be used with requests
+    # default thumbnail to be used in the video
+    # maximum message length in Telegram
+    # set timeout for subprocess
+    
+    SUDO_USERS = 1079261681
